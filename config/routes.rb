8d14db470340a0198ab7e949ab0c857a3fec9578
Bennett Chidoro <bchidoro9@gmail.com>
2020-04-15 06:58:20 -0400

    feat: nest reviews under movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :reviews
   # verb "url" => "name_of_controller#name_of_action"
   root 'movies#index'
-  resources :movies
+
+  resources :movies do
+    resources :reviews
+  end
 end
