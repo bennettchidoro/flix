25da032e8370c0c9e1cb0a6259f9c8c94f7d7970
Bennett Chidoro <bchidoro9@gmail.com>
2020-04-23 14:50:53 -0400

    feat: add custom route /signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
   # verb "url" => "name_of_controller#name_of_action"
   root 'movies#index'
+  get "/signup" => "users#new" 
 
   resources :movies do
     resources :reviews
   end
+
+  resources :users
 end
