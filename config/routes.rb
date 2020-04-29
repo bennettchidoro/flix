fb0e4e20f0755b4688ce0b126fd7a97779ee84ea
Bennett Chidoro <bchidoro9@gmail.com>
2020-04-29 13:02:32 -0400

    feat: add session resource and custom routes for signup and signin

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   # verb "url" => "name_of_controller#name_of_action"
   root 'movies#index'
-  get "/signup" => "users#new" 
+  get "/signup" => "users#new"
+  get "/signin" => "sessions#new"
 
   resources :movies do
     resources :reviews
   end
 
   resources :users
+
+  resource :session, only: [:new, :create, :destroy]
 end
